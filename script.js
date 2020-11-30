47b2d42e9c56fb8816d9c802758486da9b88d9b6
emo-game <75253831+emo-game@users.noreply.github.com>
2020-11-30 13:44:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -255,7 +255,7 @@ Promise.all([
 
 
 
-video.addEventListener('playing',()=> {
+video.addEventListener('play',()=> {
   const canvas = faceapi.createCanvasFromMedia(video)
   document.body.append(canvas)
   const displaySize = { width: video.width, height: video.height }
